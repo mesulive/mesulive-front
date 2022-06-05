91450c6ebe0d1eea2c62e9217cd9b9128fee0e76
vetan2 <vetan2@naver.com>
2022-06-05 11:35:38 +0000

    feature/mui-theme Move muiTheme.ts & Add palette
    Update eslint
    
    Merge-request: MESULIVE-MR-2
    Merged-by: Jinho Choi <vetan2@naver.com>

---
+++ src/lib/theme/palette.ts
@@ -0,0 +1,25 @@
+import {PaletteOptions} from "@mui/material";
+
+const palette: PaletteOptions = {
+  primary: {
+    main: '#FF8009',
+    dark: '#CC6708',
+    light: '#FFA857',
+  },
+  secondary: {
+    main: '#FFB703',
+  },
+  error: {
+    main: '#EF5350',
+  },
+  grey: {
+    900: '#212121',
+    800: '#3B3B3B',
+    700: '#505050',
+    600: '#757575',
+    500: '#9B9B9B',
+    400: '#C5C5C5',
+  },
+};
+
+export default palette;
